e4822f3052b83b65bb07a6292003f48e3f91eaad
余磊 <yulei@addcn.com>
2022-03-23 22:54:18 +0800

    chore: add router

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: 'use-element-visibility-bug', component: () => import('@/views/useElementVisibilityBug.vue') },
+  ],
+})
