e098015a8a188883d774153216f3141cdf7eae3b
余磊 <yulei@addcn.com>
2022-03-24 23:54:55 +0800

    chore: center demo

---
+++ src/views/ToCenterInCSS.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>To Center In CSS</h1>
+    <p>
+      This is a simple example of how to center a div in CSS.
+    </p>
+    <div class="center">
+      <p>
+        This div is centered.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+.parent {
+  height: 300px;
+  overflow-y: auto;
+  margin-top: 300px;
+  border: 2px solid #ccc;
+}
+
+</style>
